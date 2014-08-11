6e8c44e1991a7af646076e53a5d436ab87e4669c
obara.takao <obara.takao@dena.jp>
2014-08-11 16:40:27 +0900

    follow new WebModule style

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-=========
-Compile.js
-=========
+# Compile.js [![Build Status](https://travis-ci.org/uupaa/Compile.js.png)](http://travis-ci.org/uupaa/Compile.js)
 
-![](https://travis-ci.org/uupaa/Compile.js.png)
+[![npm](https://nodei.co/npm/uupaa.compile.js.png?downloads=true&stars=true)](https://nodei.co/npm/uupaa.compile.js/)
 
 Closure Compiler wrapper.
 
-# Document
+## Document
 
-- [WebModule](https://github.com/uupaa/WebModule) ([Slide](http://uupaa.github.io/Slide/slide/WebModule/index.html))
-- [Development](https://github.com/uupaa/WebModule/wiki/Development)
 - [Compile.js wiki](https://github.com/uupaa/Compile.js/wiki/Compile)
+- [Closure Compiler Service](http://closure-compiler.appspot.com/home)
+- [WebModule](https://github.com/uupaa/WebModule)
+    - [Slide](http://uupaa.github.io/Slide/slide/WebModule/index.html)
+    - [Development](https://github.com/uupaa/WebModule/wiki/Development)
 
+## How to use
 
-# How to use
+### Install
 
 ```sh
 $ npm install -g uupaa.compile.js
 ```
 
+### Node.js
+
 ```js
-// for Node.js
 var Compile = require("uupaa.compile.js");
 var inputFile = "./input.js";
 var outputFile = "./output.js";
--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-// @name: Compile.js
-// @require: fs, child_process, path
-// @cutoff: @node
-//
-// Closure Compiler Service
-//  http://closure-compiler.appspot.com/home
-
 (function(global) {
+"use strict";
 
-// --- variable --------------------------------------------
+// --- dependency modules ----------------------------------
 var fs = require("fs");
+var path = require("path");
 var childProcess = require("child_process");
 
-// --- define ----------------------------------------------
-// --- interface -------------------------------------------
-function Compile() { // @help: Compile
+// --- define / local variables ----------------------------
+//var _runOnNode = "process" in global;
+//var _runOnWorker = "WorkerLocation" in global;
+//var _runOnBrowser = "document" in global;
+
+// --- class / interfaces ----------------------------------
+function Compile() {
 }
 
+//{@dev
 Compile["repository"] = "https://github.com/uupaa/Compile.js";
+//}@dev
+
 Compile["exec"] = Compile_exec; // Compile.exec(input:PathString, output:PathString,
                                 //              options:String, callback:Function):void
 
-// --- implement -------------------------------------------
-function Compile_exec(input,      // @arg PathString:
-                      output,     // @arg PathString:
-                      options,    // @arg String:
-                      callback) { // @arg Function: callback(err, stdout, stderr):void
-                                  // @help: Compile.exe
-    var jar = require("path").join(__dirname, 'vendor/compiler.jar');
+// --- implements ------------------------------------------
+function Compile_exec(input,      // @arg PathString
+                      output,     // @arg PathString
+                      options,    // @arg String
+                      callback) { // @arg Function - callback(err, stdout, stderr):void
+    var jar = path.join(__dirname, 'vendor/compiler.jar');
 
     var command = "java -jar " + jar +
                   " --js_output_file " + output +
@@ -38,17 +39,20 @@ function Compile_exec(input,      // @arg PathString:
     });
 }
 
-// --- export ----------------------------------------------
-//{@node
-if (global["process"]) { // node.js
+// --- validate / assertions -------------------------------
+//{@dev
+//function $valid(val, fn, hint) { if (global["Valid"]) { global["Valid"](val, fn, hint); } }
+//function $type(obj, type) { return global["Valid"] ? global["Valid"].type(obj, type) : true; }
+//function $keys(obj, str) { return global["Valid"] ? global["Valid"].keys(obj, str) : true; }
+//function $some(val, str, ignore) { return global["Valid"] ? global["Valid"].some(val, str, ignore) : true; }
+//function $args(fn, args) { if (global["Valid"]) { global["Valid"].args(fn, args); } }
+//}@dev
+
+// --- exports ---------------------------------------------
+if (global["process"]) {
     module["exports"] = Compile;
 }
-//}@node
-if (global["Compile"]) {
-    global["Compile_"] = Compile; // already exsists
-} else {
-    global["Compile"]  = Compile;
-}
+global["Compile" in global ? "Compile_" : "Compile"] = Compile; // switch module. http://git.io/Minify
 
-})((this || 0).self || global);
+})((this || 0).self || global); // WebModule idiom. http://git.io/WebModule
 
